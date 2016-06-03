d118ba2deab47a400b5fd3971f768d8c1aad81ae
Afanasev Dmitriy <afanavt@gmail.com>
2016-06-03 11:53:58 +0600

    correct fixture

--- fixtures/data/users.php
+++ fixtures/data/users.php
@@ -8,9 +8,9 @@
 
 return [
     ['id'=> 1, 'name' => 'user1', 'balance' => '1234.67'],
-    ['id'=> 2, 'name' => 'user1', 'balance' => '10234.7'],
-    ['id'=> 3, 'name' => 'user1', 'balance' => '12034.54'],
-    ['id'=> 4, 'name' => 'user1', 'balance' => '2034.01'],
-    ['id'=> 5, 'name' => 'user1', 'balance' => '343'],
-    ['id'=> 6, 'name' => 'user1', 'balance' => '4345'],
+    ['id'=> 2, 'name' => 'user2', 'balance' => '10234.7'],
+    ['id'=> 3, 'name' => 'user3', 'balance' => '12034.54'],
+    ['id'=> 4, 'name' => 'user4', 'balance' => '2034.01'],
+    ['id'=> 5, 'name' => 'user5', 'balance' => '343'],
+    ['id'=> 6, 'name' => 'user6', 'balance' => '4345'],
 ];
